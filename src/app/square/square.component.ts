42da2594819bd6c693262a172fa4cea807a9cd39
JayMartMedia <Jay@JayMartMedia.com>
2022-05-07 13:52:31 -0400

    chore(square): refactor square to use button rather than p tag

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,12 +3,12 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <p>
+    <button>
       {{ value }}
-    </p>
+    </button>
   `,
   styles: [
-    'p {text-align: center; font-size: 170px; margin: 0px; line-height: 200px;}'
+    'button {text-align: center; font-size: 170px; height:200px; line-height: 200px; width: 200px;}'
   ]
 })
 export class SquareComponent {
