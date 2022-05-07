e62055f177f07451bdc4c76417ecbcb1eab93d27
JayMartMedia <Jay@JayMartMedia.com>
2022-05-07 14:17:05 -0400

    chore(style): add nb styles to buttons

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,12 +3,13 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>
-      {{ value }}
-    </button>
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton *ngIf="value === 'X'" status="primary">{{value}}</button>
+    <button nbButton *ngIf="value === 'O'" status="success">{{value}}</button>
   `,
   styles: [
-    'button {text-align: center; font-size: 170px; height:200px; line-height: 200px; width: 200px;}'
+    'button {text-align: center; font-size: 170px !important; height:200px; line-height: 200px; width: 200px;}'
+
   ]
 })
 export class SquareComponent {
